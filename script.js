1e59f0deedbc3d970e00bbb019403ec524fcd721
ElisNiya <50265309+ElisNiya@users.noreply.github.com>
2021-01-03 16:57:44 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+let int = setIntervan(blurring, 30)
+
+function blurring(){
+    load++
+    
+    if(load > 99){
+        clearInterval(int);
+    }
+    loadText.innerText = `${load}%`
+    loadText.getElementsByClassName.opacity = scale(load, 0, 100, 1, 0)
+    bg.style.filter = `blur(${scale(load, 0, 100, 30,0)}px)`
+
+    //blurrs from 0 to 100%
+  }
+  const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+  }
